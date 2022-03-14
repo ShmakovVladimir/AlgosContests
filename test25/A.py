60843f541cdaa1a88e853e3f44885d2986e4d30f
ShmakovVladimir <kaktusation@gmil.com>
2022-03-14 16:40:13 +0300

    Неправильный ответ

--- test25/A.py
+++ test25/A.py
@@ -2,9 +2,11 @@ graphMatrix = []
 for i in range(int(input())):
     graphMatrix.append(list(map(bool,input().split())))
 for i in range(len(graphMatrix)):
-    if 
-    for j in range(min(i,len(graphMatrix)-i)):
-        if not graphMatrix[i][i+j]*graphMatrix[i][i-j]:
+    if graphMatrix[i][i]:
+        print("NO")
+        exit()
+    for j in range(i,len(graphMatrix)):
+        if graphMatrix[i][j]!=graphMatrix[j][i]:
             print("NO")
             exit()
 print("YES")

e67d44a32f48aa703f4e2555aba41b5ddd744b7e
ShmakovVladimir <kaktusation@gmil.com>
2022-03-14 23:12:48 +0300

    Задача A работает

--- test25/A.py
+++ test25/A.py
@@ -1,12 +1,9 @@
 graphMatrix = []
 for i in range(int(input())):
-    graphMatrix.append(list(map(bool,input().split())))
-for i in range(len(graphMatrix)):
-    if graphMatrix[i][i]:
-        print("NO")
-        exit()
-    for j in range(i,len(graphMatrix)):
-        if graphMatrix[i][j]!=graphMatrix[j][i]:
+    graphMatrix.append([int(i) for i in input().split()])
+for j in range(len(graphMatrix)):
+    for k in range(i,len(graphMatrix)):
+        if not graphMatrix[j][k]==graphMatrix[k][j] and not bool(graphMatrix[j][j]):
             print("NO")
             exit()
 print("YES")

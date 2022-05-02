c9ac8ca4e446278f084a58a7da588073d0619186
ShmakovVladimir <kaktusation@gmil.com>
2022-05-02 03:22:37 +0300

    Решение B

--- test211/B.py
+++ test211/B.py
@@ -1,9 +1,18 @@
-_,_ = map(int,input().split())
-stones = list(map(int,input().split()))
-xorSum = stones[0]
-for i in range(1,len(stones)):
-    xorSum^=stones[i]
-if xorSum == 0:
-    print("YES")
-    exit()
+
+def win(stones: list)->bool:
+    ans = stones[0]
+    for i in range(1,len(stones)):
+        ans^=stones[i]
+    return ans==0
+
+heapQ,maxPick = map(int,input().split())
+startState = list(map(int,input().split()))
+
+for heapNum in range(len(startState)):
+    newState = startState.copy()
+    for _ in range(maxPick+1):
+        if win(newState):
+            print("YES")
+            exit()
+        newState[heapNum]-=1
 print("NO")
